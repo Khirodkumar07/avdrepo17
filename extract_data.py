b6117cc39c827b4943ed26a294e5c4410798ed1c
Khirod Kumar Jati <kumarpapu192@gmail.com>
2026-05-23 00:39:40 +0530

    jenkin script added

--- extract_data.py
+++ extract_data.py
@@ -1,16 +1,31 @@
+# import pandas as pd
+
+# print("Extract Data")
+
+# # Sample data
+# data = {
+#     'Id': [101, 102, 103],
+#     'Name': ['Ram', 'Raj', 'Raja'],
+#     'Age': [29, 34, 42]
+# }
+
+# # Create DataFrame
+# df = pd.DataFrame(data)
+
+# # Display the DataFrame
+# print(df)
+
+
+#session-17 content
+
+import requests
 import pandas as pd
 
-print("Extract Data")
+response = requests.get("https://jsonplaceholder.typicode.com/users")
+data = response.json()
 
-# Sample data
-data = {
-    'Id': [101, 102, 103],
-    'Name': ['Ram', 'Raj', 'Raja'],
-    'Age': [29, 34, 42]
-}
-
-# Create DataFrame
 df = pd.DataFrame(data)
+df = df[["id", "name", "email"]]
 
-# Display the DataFrame
 print(df)
+
